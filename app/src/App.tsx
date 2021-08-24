6a261c5c65bf6d7421c5abadbd34162bd3846f24
Oleg <krapniknil1213143@gmail.com>
2021-08-24 19:45:25 +0300

    added base store

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux'; 
 
 export const App = () => {
+  const message = useSelector((state) => state)
+  console.log(message)
   return (
     <div>
-      Starting project
+      hello
     </div>
   );
 }
---
+++ app/src/store/reducers/movieReducers/movieReducer.ts
@@ -0,0 +1,13 @@
+const initialState = {
+    movies: [],
+    message: 'Hello store'
+}
+
+export const movieReducer = (state = initialState , action:any) => {
+    switch(action.type) {
+        default: return state
+ }
+}
+
+
+
